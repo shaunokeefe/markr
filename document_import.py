8dff3a4ad56232f0dfcd6da7d913a52915875df4
shaunokeefe <shaun.okeefe.0@gmail.com>
2018-02-11 11:00:31 +1100

    retrieve non-duplicate users before processing

--- document_import.py
+++ document_import.py
@@ -1,3 +1,4 @@
+import collections
 import sys
 
 import xmltodict
@@ -7,6 +8,33 @@ class MarkrDocument(object):
     def __init__(self, body):
         self.raw = body
         self.document = xmltodict.parse(body)
+
+    def available_marks(self, student):
+        # TODO(shauno): more situation specific error handling - or ok because
+        # this is just for testing?
+        # TODO(shauno): separate student class
+        return int(student['summary-marks']['@available'])
+
+    def _unique_students(self):
+        students = {}
+        result_list = self.document['mcq-test-results']['mcq-test-result']
+
+        if isinstance(result_list, collections.OrderedDict):
+            result_list = [result_list]
+
+        def _available(student):
+            return int(student['summary-marks']['@available'])
+
+        for result in result_list:
+            student_number = result['student-number']
+            available_marks = _available(result)
+
+            if student_number in students:
+                if _available(students[student_number]) >= available_marks:
+                    continue
+
+            students[student_number] = result
+        return students
 
 
 def import_function(filename):
--- test/test_markr_document.py
+++ test/test_markr_document.py
@@ -15,3 +15,31 @@ class TestMarkrDocument(object):
         assert 'mcq-test-results' in document.document
         results_list = document.document['mcq-test-results']['mcq-test-result']
         assert len(results_list) == 100
+
+    def test_gets_available_marks(self):
+        contents = open('test/data/single_result.xml').read()
+        student = {'summary-marks': {'@available': 20}}
+
+        document = document_import.MarkrDocument(contents)
+
+        assert document.available_marks(student) == 20
+
+    def test_gets_unique_students_duplicates(self):
+        student_number = '002299'
+        contents = open('test/data/duplicate_user_1.xml').read()
+
+        document = document_import.MarkrDocument(contents)
+        students = document._unique_students()
+
+        assert len(students) == 1
+        student = students[student_number]
+        assert document.available_marks(student) == 21
+
+        contents = open('test/data/duplicate_user_2.xml').read()
+
+        document = document_import.MarkrDocument(contents)
+        students = document._unique_students()
+
+        assert len(students) == 1
+        student = students[student_number]
+        assert document.available_marks(student) == 21
